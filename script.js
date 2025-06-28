0a1a7cac048d6435d1bbc3c6f6902981916fc09d
Eibeth Delgado <eibeth.delgado@gmail.com>
2025-06-27 23:16:45 -0400

    Agregue validaciones al formulario y mensaje de exito

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-$("#boton_realizar_pago").click(function(){
+$("#boton_realizar_pago").click(function () {
+    let nombre = $("#inputNombreDelCliente").val().trim();
+    let servicio = $("#seleccionarServicio").val();
+    let fecha = $("#fecha").val();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa el nombre del cliente.");
+        return;
+    }
+
+    if (!servicio || servicio === "Selecciona un servicio") {
+        alert("Por favor, selecciona un tipo de reparación.");
+        return;
+    }
+
+    if (fecha === "") {
+        alert("Por favor, agenda una fecha para la cita.");
+        return;
+    }
     $("#modal_pago").fadeIn();
+});
+
+$("#form_pago").submit(function(e){
+    e.preventDefault();
+    $("#mensajeExito").removeClass("d-none").hide().fadeIn()
+    
+    setTimeout(function(){
+        $("#modal_pago").fadeOut();
+    }, 1000);
+
+    setTimeout(function(){
+        $("#mensajeExito").fadeOut(function(){
+            $(this).addClass("d-none")
+        })
+    }, 3000)
+
+    $("form")[0].reset();
+    $("#form_pago")[0].reset()
 })
-
-
 
 $("#modal_pago").on("click", function(e){
     if (e.target === this){
